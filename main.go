853be50ce1d1641261c659850ab3609fd17d9346
Vaibhav Kamra <vkamra@alcion.ai>
2023-03-29 15:32:16 -0700

    Memory profiling

--- main.go
+++ main.go
@@ -10,7 +10,10 @@ Use 'kopia help' to see more details.
 package main
 
 import (
+	"fmt"
 	"os"
+	"runtime"
+	"runtime/pprof"
 
 	"github.com/alecthomas/kingpin/v2"
 
@@ -64,6 +67,20 @@ Commands (use --help-full to list all commands):
 `
 
 func main() {
+	defer func() {
+		PrintMemUsage()
+		f, err := os.Create("mem.prof")
+		if err != nil {
+			fmt.Print("could not create memory profile: ", err)
+			return
+		}
+		defer f.Close() // error handling omitted for example
+		runtime.GC()    // get up-to-date statistics
+		if err := pprof.WriteHeapProfile(f); err != nil {
+			fmt.Print("could not write memory profile: ", err)
+			return
+		}
+	}()
 	app := cli.NewApp()
 	kp := kingpin.New("kopia", "Kopia - Fast And Secure Open-Source Backup").Author("http://kopia.github.io/")
 
@@ -75,4 +92,18 @@ func main() {
 
 	app.Attach(kp)
 	kingpin.MustParse(kp.Parse(os.Args[1:]))
+}
+
+func PrintMemUsage() {
+	var m runtime.MemStats
+	runtime.ReadMemStats(&m)
+	// For info on each, see: https://golang.org/pkg/runtime/#MemStats
+	fmt.Printf("Alloc = %v MiB", bToMb(m.Alloc))
+	fmt.Printf("\tTotalAlloc = %v MiB", bToMb(m.TotalAlloc))
+	fmt.Printf("\tSys = %v MiB", bToMb(m.Sys))
+	fmt.Printf("\tNumGC = %v\n", m.NumGC)
+}
+
+func bToMb(b uint64) uint64 {
+	return b / 1024 / 1024
 }
